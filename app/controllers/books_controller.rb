680b5978da999f57d8127e6575a7fdecd75e3e21
榎本拓真 <t3460515@gmail.com>
2020-08-13 17:53:30 +0900

    4-8-1[top]

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,18 @@
+class BooksController < ApplicationController
+  
+  def top
+
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+
+	get 'top' => 'books#top'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    resources :blogs
 end
